aab05322b2dc0c18366a23ff3165dcf6b381f13c
aadhil <m.aadhilaslam@gmail.com>
2025-07-06 13:07:10 +0530

    baseUrl

---
+++ lib/data/services/api_services.dart
@@ -0,0 +1,6 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class ApiService {
+  final String baseUrl = 'https://6868ca64d5933161d70c7025.mockapi.io/';
+}
--- lib/data/services/job_service.dart
+++ lib/data/services/job_service.dart
@@ -2,16 +2,18 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:job_lisiting/data/models/job.dart';
+import 'package:job_lisiting/data/services/api_services.dart';
 
 class JobServices{
   final http.Client client;
+  final ApiService _apiServices = ApiService();
 
   JobServices(this.client);
 
   Future<List<Job>> fetchJobs() async {
-    final response = await client.get(Uri.parse('https://6868ca64d5933161d70c7025.mockapi.io/api/jobs'));
+    final response = await client.get(Uri.parse('${_apiServices.baseUrl}api/jobs'));
+    // ('https://6868ca64d5933161d70c7025.mockapi.io/api/jobs'));
 
-    print(response.body);
     if (response.statusCode == 200) {
       List<dynamic> jobList = json.decode(response.body);
       return jobList.map((jobData) => Job.fromJson(jobData)).toList();
