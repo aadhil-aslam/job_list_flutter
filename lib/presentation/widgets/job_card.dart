c212de6cccd4022104c75e658c4672919212a5dc
aadhil <m.aadhilaslam@gmail.com>
2025-07-06 14:22:09 +0530

    Expanded & ellipsis

--- lib/presentation/widgets/job_card.dart
+++ lib/presentation/widgets/job_card.dart
@@ -24,78 +24,74 @@ class _JobCardState extends State<JobCard> {
       child: Material(
         color: Colors.transparent,
         child: ListTile(
-        title: Text(widget.job.title, style: TextStyle(fontSize: 16),),
-        subtitle: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(height: 8),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                SizedBox(
-                  // height: 28.0,
-                  // width: 30.0,
-                  child: Icon(Icons.location_city, color: Colors.teal),
-                ),
-                Text(
-                  widget.job.company,
-                  style: TextStyle(fontSize: 16),
-                ),
-                SizedBox(width: 8),
-                // Icon(icon, size: 20, color: Colors.teal)
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                SizedBox(height: 40.0,),
-                SizedBox(
-                  // height: 28.0,
-                  // width: 30.0,
-                  child: Icon(Icons.location_on, color: Colors.teal),
-                ),
-                Text(
-                  widget.job.location,
-                  style: TextStyle(fontSize: 14),
-                ),
-                SizedBox(width: 8),
-                // Icon(icon, size: 20, color: Colors.teal)
-              ],
-            ),
-          ],
-        ),
-        
-        // Text('${widget.job.company} - ${widget.job.location}'),
-        trailing: BlocBuilder<FavoriteJobsBloc, FavoriteJobsState>(
-          builder: (context, state) {
-            final isFavorited = state is FavoriteJobsLoaded &&
-                state.favoriteJobIds.contains(widget.job.id);
-
-            return IconButton(
-              icon: AnimatedSwitcher(
-                duration: const Duration(milliseconds: 300),
-                transitionBuilder: (child, animation) => ScaleTransition(
-                  scale: animation,
-                  child: child,
-                ),
-                child: Icon(
-                  isFavorited ? Icons.favorite : Icons.favorite_border,
-                  key: ValueKey(isFavorited),
-                  color: isFavorited ? Colors.red : null,
-                ),
+          title: Text(widget.job.title, style: TextStyle(fontSize: 16),),
+          subtitle: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 5),
+              Row(
+                children: [
+                  Icon(Icons.location_city, color: Colors.teal, size: 20.0, ),
+                  SizedBox(width: 8),
+                  Expanded(
+                    child: Text(
+                      widget.job.company,
+                      style: TextStyle(fontSize: 16),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                ],
               ),
-              onPressed: () {
-                context.read<FavoriteJobsBloc>().add(ToggleFavoriteJob(widget.job.id));
-              },
-            );
+              Row(
+                children: [
+                SizedBox(height: 30.0,),
+                  
+                  Icon(Icons.location_on, color: Colors.teal, size: 20.0,),
+                  SizedBox(width: 8),
+                  Expanded(
+                    child: Text(
+                      widget.job.location,
+                      style: TextStyle(fontSize: 14),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          
+        // Text('${widget.job.company} - ${widget.job.location}'),
+          trailing: BlocBuilder<FavoriteJobsBloc, FavoriteJobsState>(
+            builder: (context, state) {
+              final isFavorited = state is FavoriteJobsLoaded &&
+                  state.favoriteJobIds.contains(widget.job.id);
+
+              return IconButton(
+                icon: AnimatedSwitcher(
+                  duration: const Duration(milliseconds: 300),
+                  transitionBuilder: (child, animation) => ScaleTransition(
+                    scale: animation,
+                    child: child,
+                  ),
+                  child: Icon(
+                    isFavorited ? Icons.favorite : Icons.favorite_border,
+                    key: ValueKey(isFavorited),
+                    color: isFavorited ? Colors.red : null,
+                  ),
+                ),
+                onPressed: () {
+                  context.read<FavoriteJobsBloc>().add(ToggleFavoriteJob(widget.job.id));
+                },
+              );
+            },
+          ),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (_) => JobDetailScreen(job: widget.job)));
           },
         ),
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (_) => JobDetailScreen(job: widget.job)));
-        },
-      ),
-    ));
+      )
+    );
   }
 }
